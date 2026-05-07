0af16075302ffbb23f21f5fafb8877691acd40c7
Teo <mateoq152006@gmail.com>
2026-05-07 16:41:49 -0500

    feature(20-auth-api): se crea las clase nesesarias para iniciar secion como LoginRequestDto y LoginResponseDto

---
+++ ScheduleApp/ScheduleApp.Application/DTOs/LoginRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+// ScheduleApp.Application/DTOs/LoginRequestDto.cs
+namespace ScheduleApp.Application.DTOs;
+
+public class LoginRequestDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ ScheduleApp/ScheduleApp.Application/DTOs/LoginResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// ScheduleApp.Application/DTOs/LoginResponseDto.cs
+namespace ScheduleApp.Application.DTOs;
+
+public class LoginResponseDto
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string TokenType { get; set; } = "Bearer";
+    public DateTime ExpiresAt { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
