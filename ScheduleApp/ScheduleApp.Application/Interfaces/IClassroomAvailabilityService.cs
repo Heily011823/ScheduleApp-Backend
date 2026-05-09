7580e7830ba1d0c5771493c5b370dd2145977355
Maria Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2026-05-09 18:09:52 -0500

    feat: implementar la interfaz IClassroomAvailabilityService.cs

---
+++ ScheduleApp/ScheduleApp.Application/Interfaces/IClassroomAvailabilityService.cs
@@ -0,0 +1,21 @@
+﻿using ScheduleApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScheduleApp.Application.Interfaces
+{
+    public interface IClassroomAvailabilityService
+    {
+        bool IsAvailable(
+            int classroomId,
+            DayOfWeek day,
+            TimeSpan startTime,
+            TimeSpan endTime
+        );
+
+        string SaveAssignment(
+            ClassroomAssignment newAssignment
+        );
+    }
+}
