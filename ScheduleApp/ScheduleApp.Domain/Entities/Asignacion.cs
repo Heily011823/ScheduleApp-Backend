12d80761d07a7ede5649d865e65443dc2d0fbaf0
Juan Hernandez <juanestebanhernandezmartinez@gmail.com>
2026-05-10 08:52:57 -0500

    feat: Se implementa la validación de cruces de horarios

--- ScheduleApp/ScheduleApp.Domain/Entities/Asignacion.cs
+++ ScheduleApp/ScheduleApp.Domain/Entities/Asignacion.cs
@@ -8,8 +8,17 @@ namespace ScheduleApp.Domain.Entities
     public class Asignacion
     {
         public int Id { get; set; }
+
         public string Docente { get; set; }
+
         public string Materia { get; set; }
-        public DateTime FechaHora { get; set; }
+
+        public string Aula { get; set; }
+
+        public DayOfWeek Dia { get; set; }
+
+        public TimeSpan HoraInicio { get; set; }
+
+        public TimeSpan HoraFin { get; set; }
     }
 }
