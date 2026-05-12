16a3146f46857665c504dcec6692261e189ac48f
Teo <mateoq152006@gmail.com>
2026-05-12 17:40:39 -0500

    feat(40-peristencia): crea una clase que haga la refrescaccion de tokenes de los usarion

---
+++ ScheduleApp/ScheduleApp.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// ScheduleApp.Domain/Entities/RefreshToken.cs
+namespace ScheduleApp.Domain.Entities;
+
+public class RefreshToken
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsRevoked { get; set; } = false;
+
+    // Foreign Key
+    public Guid UserId { get; set; }
+    // Navegación
+    public User User { get; set; } = null!;
+
+    // Propiedades calculadas
+    public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+    public bool IsActive => !IsRevoked && !IsExpired;
+}
