a090842308de46ed83c301be7c1d89cbbcde6fb4
Teo <mateoq152006@gmail.com>
2026-05-16 14:15:32 -0500

    feature(Crud-docentes): se crea la entidad de docente y se creo unas Dtos

---
+++ src/ScheduleApp.Application/DTOs/UpdateTeacherDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScheduleApp.Application.DTOs
+{
+    internal class UpdateTeacherDto
+    {
+    }
+}
---
+++ src/ScheduleApp.Domain/Entities/Teacher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScheduleApp.Domain.Entities
+{
+    class Teacher
+    {
+    }
+}
