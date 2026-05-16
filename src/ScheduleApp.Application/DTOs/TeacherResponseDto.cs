9dc1d08330fbb633de8184dae0dac16e940afa66
Teo <mateoq152006@gmail.com>
2026-05-16 15:14:27 -0500

    feature(Crud-docentes): se inserta migraciones paar que funcione el trabjo

--- src/ScheduleApp.Application/DTOs/TeacherResponseDto.cs
+++ src/ScheduleApp.Application/DTOs/TeacherResponseDto.cs
@@ -5,10 +5,9 @@ using System.Text;
 // src/ScheduleApp.Application/DTOs/TeacherResponseDto.cs
 namespace ScheduleApp.Application.DTOs;
 
-/// Autor:  Mateo Quintero 
+/// Autor: Mateo Quintero
 /// Version: 0.2
-/// rama: 96-Crud-docentes
-
+/// Rama: 96-Crud-docentes
 /// <summary>
 /// DTO de respuesta con los datos públicos de un docente.
 /// Es lo que retorna la API en todas las operaciones.
@@ -24,5 +23,4 @@ public class TeacherResponseDto
     public bool IsActive { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
-}
 }
